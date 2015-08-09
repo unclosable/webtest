ec6dd2df98d1ef630b92b67c9368bb139553b740
zhengwei <zhengweiSE@gmail.com>
2015-08-09 17:33:56 +0800

    一些关于spring过滤器的实验项目，好低端啊

--- webtest/src/main/java/com/unclosable/webtest/controller/HelloWorldController.java
+++ webtest/src/main/java/com/unclosable/webtest/controller/HelloWorldController.java
@@ -1,18 +1,25 @@
 package com.unclosable.webtest.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.unclosable.webtest.test.testBean;
+
 @Controller
 public class HelloWorldController {
+	@Resource
+	testBean testBean;
 
 	String message = "Welcome to Spring MVC!";
 
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		testBean.doSomething();
 		ModelAndView mv = new ModelAndView("test");
 		mv.addObject("message", message);
 		mv.addObject("name", name);
---
+++ webtest/src/main/java/com/unclosable/webtest/test/testBean.java
@@ -0,0 +1,12 @@
+package com.unclosable.webtest.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class testBean {
+
+	public void doSomething() {
+		System.out.println("123");
+	}
+
+}
